ffbc41cf4b1e971f93ce81300230853b1917cfe5
Azalius <mateo1998@live.fr>
2020-11-17 13:58:23 +0100

    small fix

--- server.py
+++ server.py
@@ -3,7 +3,6 @@
 import socket
 import time
 
-HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
 def get_time_wait():
@@ -15,7 +14,7 @@ def get_time_wait():
 
 while True:
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
+        s.bind(('', PORT))
         s.listen()
         conn, addr = s.accept()
         with conn:
