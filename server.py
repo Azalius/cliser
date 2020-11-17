353cafc267750f62e74f273cbe097555612e5738
Azalius <mateo1998@live.fr>
2020-11-17 15:23:04 +0100

    compatible avec py 3.4

--- server.py
+++ server.py
@@ -16,7 +16,7 @@ def get_time_wait():
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind(('', PORT))
     while True:
-        s.listen()
+        s.listen(1)
         conn, addr = s.accept()
         with conn:
             print('Connected by', addr)
